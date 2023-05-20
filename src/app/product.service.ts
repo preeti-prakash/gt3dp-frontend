7de9d67559dae9c40be324861aecabc69bd83d8c
preeti-prakash <preetiprakashkandregula@gmail.com>
2023-05-20 18:56:36 -0400

    first commit

---
+++ src/app/product.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http:HttpClient) { }
+
+  //add new product
+  addProduct(url: string, requestBody: any):Observable<any>{
+    return this.http.post(url, requestBody);
+  }
+
+  //get all the list of products added
+  getProducts():Observable<any>{
+    return this.http.get(`http://localhost:8080/products`);
+  }
+
+  deleteProduct(id: any){
+      return this.http.delete(`http://localhost:8080/products/${id}`);
+  }
+
+  updateProduct(id: any, requestBody: any){
+    return this.http.put(`http://localhost:8080/products/${id}`, requestBody);
+  }
+}
